ec83550ac4eede9b1cf69585ba569e9793c945cd
Prashant Singh <134587691+prashantsingh05@users.noreply.github.com>
2023-08-16 21:02:22 +0530

    Add files via upload

---
+++ String5.java
@@ -0,0 +1,8 @@
+public class String5 {
+    public static void main(String[] args) {
+        // String 
+        // substring
+        String name = "Prashant and Nishant";
+        System.out.println(name.substring(0,20));
+    }
+}
